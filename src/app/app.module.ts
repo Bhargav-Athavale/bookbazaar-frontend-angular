5180000d2f3ad51bcb2cca368f855afc9ceaf965
Bhargav-Athavale <bhargav.athavale@gmail.com>
2024-08-03 19:34:34 +0100

    update to angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BookService } from './services/book-service/book.service';
 import { MatIconModule } from '@angular/material';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    RoutingComponents,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule, 
-    FormsModule,
-    ReactiveFormsModule,
-    MatIconModule 
-  ],
-  providers: [BookService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        TopBarComponent,
+        RoutingComponents,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatIconModule], providers: [BookService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
